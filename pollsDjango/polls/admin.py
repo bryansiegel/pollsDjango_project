6c9e2271ff8a97ce78ad51663daa8096fbaa5ea0
Bryan Siegel <bryansiegel@gmail.com>
2020-03-31 07:50:48 -0700

    change site header, title and index title info

--- pollsDjango/polls/admin.py
+++ pollsDjango/polls/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+admin.site.site_header = "Polls Django"
+admin.site.site_title = "Polls Django Admin Area"
+admin.site.index_title = "Welcome to Polls Django Admin Area"
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
